81be19ed8672d24927989e127ea73654ca57827f
12201412-zubair <113511809+zubair647@users.noreply.github.com>
2024-08-21 13:04:55 +0530

    Add files via upload

---
+++ charecter.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val letter : Char
+    letter = 'A'
+    println("$letter")
+    print('\n')
+    print('\$')
+    print('\\')
+}
---
+++ class2.kt
@@ -0,0 +1,59 @@
+//fun main(){
+//    val a=10
+//    val b=20
+//    val result= "The sum of $a and $b is ${a+b}"
+//    println(result);
+//}
+
+//
+
+//fun main(){
+//    val letter : Char
+//    letter = 'A'
+//    println("$letter")
+//    print('\n')
+//    print('\$')
+//    print('\\')
+//}
+
+
+
+//usage of thripple quotes
+
+//fun main(){
+//    print("""
+//        zubair
+//        zubair
+//        zubair
+//        zubair
+//        zubair
+//    """)
+//}
+
+//fun main(){
+//    print("""
+//        Zubair
+//               Zubair
+//                      zubair
+//                            zubair
+//                            """")
+//}
+
+//fun main(){
+//    print("""
+//            Zubair
+//    Zubair          Zubair
+//            Zubair
+//    """)
+//}
+
+//Array
+
+fun main(){
+    var               marks  = arrayOf(1,2,3,4,5);
+    var words: Array<String> = arrayOf("apple","ball","water","grass");
+    var              Squares = Array(5){i-> i*i};
+    println(marks)
+    println(words)
+    println(Squares)
+}
