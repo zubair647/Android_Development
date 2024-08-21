f9281af82f9812144c59c68e8a8a2d1c919becb1
12201412-zubair <113511809+zubair647@users.noreply.github.com>
2024-08-21 13:56:29 +0530

    Update class2.kt

--- class2.kt
+++ class2.kt
@@ -49,11 +49,50 @@
 
 //Array
 
+// fun main(){
+//     var               marks  = arrayOf(1,2,3,4,5);
+//     var words: Array<String> = arrayOf("apple","ball","water","grass");
+//     var              Squares = Array(5){i-> i*i};
+//     println(marks)
+//     println(words)
+//     println(Squares)
+// }
+
+//fun main() {
+//    //Arrays
+//    val name=arrayOf("John", "Jack","fasaedsa")
+//    val firstName=name[0]
+//    val secondName=name[1]
+//    val thirdName=name[2]
+//    name[2]="Zubair"
+//    val changed = name[2];
+//    println();
+//    println("First Name in Array : ${firstName}")
+//    println("Second Name in Array: ${secondName}")
+//    println("Third Name in Array : ${thirdName}")
+//    println("Changed value from ${thirdName} to ${changed}");
+//}
+
+//Multi dimensional array - 2d array
+//fun main(){
+//    var matrix:Array<Array<Int>> = arrayOf((arrayOf(1,2,3)), arrayOf(3,4,5), arrayOf(5,6,7));
+//    println("elements at row 0, colum 1: ${matrix[0][1]}")
+//}
+
+//type casting
+//fun main(){
+//    val x:Int = 100
+//    val y:Long = x.toLong()
+//    println(y)
+//}
+
+//safe and unsafe cast
+
 fun main(){
-    var               marks  = arrayOf(1,2,3,4,5);
-    var words: Array<String> = arrayOf("apple","ball","water","grass");
-    var              Squares = Array(5){i-> i*i};
-    println(marks)
-    println(words)
-    println(Squares)
+    var a = "Hellow";
+    var b : String = a as String;
+    println("Unsafe casting result : $b");
+
+    var c : Int = a as? Int
+    println("Unsafe casting result : $c");
 }
