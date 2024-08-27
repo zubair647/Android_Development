8413406fbcd52d6faba60d3bdb0c1e6251ec1bef
12201412-zubair <113511809+zubair647@users.noreply.github.com>
2024-08-27 13:56:34 +0530

    loops, functions

---
+++ class_5.kt
@@ -0,0 +1,71 @@
+//loops
+//".."  function
+
+//fun main(){
+//    for(i in 1..5){
+//        println("Number is ${i}");
+//    }
+//}
+//
+
+//Until function
+
+//fun main(){
+//    for(i in 1 until 5){
+//        println(i);
+//    }
+//}
+
+//revrse loop
+//fun main(){
+//    for(items in 5 downTo 1){
+//        println(items);
+//    }
+//}
+
+//reverse with step size
+//fun main(){
+//    for (items in 5 downTo 1 step 2){
+//        println(items);
+//    }
+//}
+
+//iterating over a list
+//fun main(){
+//    val fruits = listOf("banana", "avocado", "apple", "kiw")
+//    for(fruit in fruits.indices) {
+//        println("$fruit : ${fruits[fruit]}")
+//    }
+//}
+
+//fun main(){
+//    var i=5;
+//    while (i>0){
+//        println("value is: $i")
+//        i--
+//    }
+//}
+
+//fun main(){
+//    var i=5;
+//    do{
+//        println(i);
+//        i--;
+//    } while (i>5)
+//}
+
+//fun main(){
+//    var i=1;
+//    do {
+//        println(i);
+//        i++
+//    } while (i<6)
+//}
+
+//functions
+fun demo(){
+    println("Hello World")
+}
+fun main(){
+    demo();
+}
